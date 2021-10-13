d04eb27995cca3887817abf2eb1a184c864b1e38
Abhishek Antaram Bagde <abagde75@gmail.com>
2021-10-13 23:25:19 +0530

    added new app

--- MyPro/asgi.py
+++ MyPro/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyPro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypro.settings')
 
 application = get_asgi_application()
